88b35ce17307d7b70f48381d208c2c0eef89ac2e
Maksim <familinov@gmail.com>
2020-08-16 01:11:43 +0500

    menu

--- src/app.js
+++ src/app.js
@@ -1,2 +1,19 @@
 import "./less/style.less";
 import "./image/svg/logo.svg";
+
+const menuButton = document.querySelector('.navigation__button');
+const navigation = document.querySelector('.navigation');
+
+menuButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  const menuButtonContainer = menuButton.querySelector('.nav-button');
+  const isClosed = menuButtonContainer.classList.contains('nav-button--close')
+  navigation.classList.toggle('navigation--open');
+  if (isClosed) {
+    menuButtonContainer.classList.add('nav-button--open');
+    menuButtonContainer.classList.remove('nav-button--close');
+  } else {
+    menuButtonContainer.classList.remove('nav-button--open');
+    menuButtonContainer.classList.add('nav-button--close');
+  }
+});
