908a266c9425bf103fe3e6ebad662ce5e03f9393
Maksim <familinov@gmail.com>
2020-08-30 23:08:02 +0500

    main scroll

--- src/js/scroller.js
+++ src/js/scroller.js
@@ -165,3 +165,30 @@ function debounce(func, ms) {
   }, 50))
 }
 
+//main scroll
+{
+  const scrollItems = document.querySelectorAll(`.swiper__item`);
+  const scrollButtons = document.querySelectorAll(`.swiper__slider-button`);
+
+  function setMainButtonChecked(buttons ,index) {
+    buttons.forEach((item, i) => {
+      item.classList.remove(`swiper__slider-button--active`);
+      if (i === index) {
+        item.classList.add(`swiper__slider-button--active`);
+      }
+    })
+  }
+
+  scrollButtons.forEach((button, index) => {
+
+    button.addEventListener(`click`, (evt) => {
+      const currentPosition = evt.target.dataset.buttonId;
+      const movePosition = currentPosition - 1;
+
+      scrollItems.forEach((item) => {
+        item.style.transform = `translateX(-${100 * movePosition}%)`;
+      });
+    })
+
+  });
+}
